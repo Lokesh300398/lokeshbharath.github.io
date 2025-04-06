f54577ce29fe9e1bc92a3999a82647bab768801b
Lokesh Bharath <lokeshbharath.t@gmail.com>
2025-04-06 17:48:41 +0530

    Add files via upload
    This is my first commit 4/6/2025

---
+++ script.js
@@ -0,0 +1,7 @@
+<script>
+  function showPhoneNumber() {
+    const btn = document.getElementById("contactBtn");
+    btn.innerHTML = '<i class="bi bi-telephone-fill"></i> 6300389010';
+  }
+</script>
+    
